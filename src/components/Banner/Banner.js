fa277c2cd2273ca03b8ede63bed1a00076429e7f
toum21 <pey2moo1@gmail.com>
2024-01-22 10:58:15 -0800

    updated banner styling

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Container, Typography } from '@mui/material'
+import './banner.css'
 
 const Banner = () => {
   return (
     <div className='banner'>
-      
+
+      <Container className='banner-content'>
+        <div className='tagline'>
+          <Typography
+            variant='h2'
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat"
+            }}
+          >
+            Crypto Hunter
+          </Typography>
+          <Typography
+          variant='subtitle2'
+            style={{
+              color: "darkgray",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat"
+            }}
+          >
+            Get all the info regarding yout favorite Crypto Currency
+
+          </Typography>
+        </div>
+      </Container>
+
     </div>
   )
 }
