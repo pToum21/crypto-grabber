6315bc83a3b3f0bcfb664b779ea403e16aa1bc3c
toum21 <pey2moo1@gmail.com>
2024-01-22 12:19:02 -0800

    created a searchbar inside of coinstable as well as imported the dark theme

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { CoinList } from '../config/api'
+import { CryptoState } from '../CryptoContext'
+import { Container, TextField, ThemeProvider, Typography, createTheme } from '@mui/material'
 
 const CoinsTable = () => {
-  return (
-    <div>
-      coin table
-    </div>
-  )
+    const [coins, setCoins] = useState([])
+
+    const [loading, setLoading] = useState(false)
+
+    const [search, setSearch] = useState()
+
+    const { currency } = CryptoState();
+
+    const fetchCoins = async () => {
+        setLoading(true)
+        const { data } = await axios.get(CoinList(currency));
+
+        setCoins(data);
+        setLoading(false);
+    };
+
+    console.log(coins);
+
+    useEffect(() => {
+        fetchCoins();
+    }, [currency]);
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+            type: 'dark'
+        }
+    })
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <Container style={{ textAlign: "center" }}>
+                <Typography
+                    variant='h4'
+                    style={{ margin: 18, fontFamily: "Montserrat" }}
+                >
+                </Typography>
+                <TextField 
+                label="Search for a Crypto Currency to grab.." 
+                variant='outlined'
+                style={{
+                    marginBottom: 20,
+                    width: "100%"
+                }}
+                onChange={(e)=>setSearch(e.target.value)}
+                />
+
+                
+            </Container>
+        </ThemeProvider>
+    )
 }
 
 export default CoinsTable
