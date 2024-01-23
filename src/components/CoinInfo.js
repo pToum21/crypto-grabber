f5f2143622fb0caf7c064ee5b0de886bac9f4786
toum21 <pey2moo1@gmail.com>
2024-01-23 11:26:41 -0800

    working on chart styling and displaying the chart

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CryptoState } from '../CryptoContext'
+import axios from 'axios'
+import { HistoricalChart } from '../config/api'
+import { ThemeProvider, createTheme } from '@mui/material'
+import './coininfo.css'
 
-function CoinInfo() {
+function CoinInfo({coin}) {
+
+const [historicData, setHistoricData] = useState()
+const [days, setDays] = useState(1)
+
+const { currency } = CryptoState()
+
+const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency))
+
+    setHistoricData(data.prices);
+}
+
+useEffect(() => {
+    fetchHistoricData()
+}, [currency, days])
+
+const darkTheme = createTheme({
+    pallette: {
+        main: "#fff",
+    },
+    type: "dark"
+})
+
   return (
     <div>
-      coin info
+      <ThemeProvider theme={darkTheme}>
+        <div className='coin-info-container'>
+            {/* chart */}
+            
+        </div>
+      </ThemeProvider>
     </div>
   )
 }
