c84c247c0df68095d7b2267b842269dd93dc3717
toum21 <pey2moo1@gmail.com>
2024-01-22 17:49:08 -0800

    when you click a coin in the main area it takes you to that specifc coin page

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -68,7 +68,7 @@ export default function CoinsTable() {
             },
         },
     });
-    
+
 
 
     const styles = {
--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'; 
+import { CryptoState } from '../CryptoContext'
+import {SingleCoin} from '../config/api'
+import axios from 'axios';
+import './coinpage.css'
 
 const CoinPage = () => {
+
+const { id } = useParams();
+const [coin, setCoin] = useState();
+
+const { currency, symbol } = CryptoState();
+
+const fetchCoin = async () => {
+  const { data } = await axios.get(SingleCoin(id))
+
+  setCoin(data)
+}
+
+console.log(coin)
+
+useEffect(() => {
+  fetchCoin();
+}, [])
+
   return (
-    <div>
-      coinpage
+    <div className='container'>
+      
     </div>
   )
 }
